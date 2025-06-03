8b9be5181b5e0863070b1294158a7e34c5d6d689
MauricioCastro16 <castromauricionicolas@ca.frre.utn.edu.ar>
2025-06-03 20:41:08 -0300

    otrapruebajira
    SCRUM-1 #done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Bienvenido a la aplicación de Mauricio, probando</h1>
+<h1>Bienvenido a la aplicación de Mauricio, probando, probando</h1>
 <p>Github: <a href="https://github.com/MauricioCastro16/IntegracionContinua">página de github</a></p>
